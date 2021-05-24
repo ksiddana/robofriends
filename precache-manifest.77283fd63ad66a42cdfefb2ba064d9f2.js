8cf45b566be4b5017583594803f13a090a6c8944
Karun Siddana - k0s0108 <karun.siddana@walmart.com>
2021-05-24 14:19:21 -0700

    Updates

---
+++ precache-manifest.77283fd63ad66a42cdfefb2ba064d9f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854a6bc0b5f1a2905631c6d180be4d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5297da7cc1112d6c7f6",
+    "url": "/static/css/2.587b1430.chunk.css"
+  },
+  {
+    "revision": "3d12b9f13784ed43a1d2",
+    "url": "/static/css/main.4d6722d4.chunk.css"
+  },
+  {
+    "revision": "c5297da7cc1112d6c7f6",
+    "url": "/static/js/2.4c99e3d7.chunk.js"
+  },
+  {
+    "revision": "6a29b30dd9e8ef5706396c026cb51452",
+    "url": "/static/js/2.4c99e3d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d12b9f13784ed43a1d2",
+    "url": "/static/js/main.5281deaf.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
